75fe455c2aae7b1717ba28e7fe1900482b374c0b
Alexander-2049m <a.alexander.2049@gmail.com>
2023-09-16 01:26:45 +0300

    planning how to this component

---
+++ src/components/AnimatedDots/AnimatedDots.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import AnimatedDots from './AnimatedDots';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'Graphics/AnimatedDots',
+  component: AnimatedDots,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    pointSize: { control: { type: 'range', min:0, max:240, step: 4 } },
+    pointMaxSize: { control: { type: 'range', min:0, max:240, step: 4 } },
+    distanceBetweenPoints: { control: { type: 'range', min:0, max:240, step: 4 } },
+    hoverRadius: { control: { type: 'range', min:0, max:240, step: 4 } },
+    padding: { control: { type: 'range', min:0, max:240, step: 4 } },
+    defaultColor: { control: 'color' },
+    hoverColor: { control: 'color' },
+    isHoverEffect: { control: 'boolean' },
+    isColorChange: { control: 'boolean' },
+    isSizeChange: { control: 'boolean' },
+    isGradientAnimation: { control: 'boolean' },
+  },
+} satisfies Meta<typeof AnimatedDots>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+    args: {},
+};
---
+++ src/components/AnimatedDots/AnimatedDots.tsx
@@ -0,0 +1,118 @@
+import { useRef } from 'react';
+
+interface Props {
+    pointSize?: number;
+    pointMaxSize?: number;
+    distanceBetweenPoints?: number | 'auto';
+    hoverRadius?: number;
+    padding?: number;
+    defaultColor?: string;
+    hoverColor?: string;
+    isHoverEffect?: boolean;
+    isColorChange?: boolean;
+    isSizeChange?: boolean;
+    isGradientAnimation?: boolean;
+}
+
+const AnimatedDots: React.FC<Props> = ({
+    pointSize = 16,
+    pointMaxSize = 32,
+    distanceBetweenPoints = 16,
+    hoverRadius = 180,
+    padding = 24,
+    defaultColor = '#ECECEC',
+    hoverColor = '#B7B7B7',
+    isHoverEffect = true,
+    isColorChange = true,
+    isSizeChange = true,
+    isGradientAnimation = true
+}) => {
+    const ref = useRef(null);
+
+    /* breakdown
+    * pointSize = 16
+    * distanceBetweenPoints = 16
+    * block { width: 360, height: 300 }
+    * 
+    * Math.floor( width / ( pointSize + distanceBetweenPoints ) )
+    * we can calculate that we can fit 11 points in a row
+    * and the same thing we can do with column calculation by using height instead of width
+    * 
+    * better calculation would be by doing a "while loop"
+    * because we are counting spare margin at the end
+    * 
+    * example width / ( pointSize + distanceBetweenPoints )
+    * example [point, margin, point, margin, point, margin]
+    * 
+    * so we do something like this
+    * 
+    * let fits = true;
+    * let size = 0;
+    * let state = 1; // 1 - point | 0 - distanceBetween
+    * let amount = 0;
+    * while(fits) {
+    *   if(state === 1) {
+    *       state = 0;
+    *       if( ( size + pointSize ) < width ) {
+    *           size += pointSize;
+    *           amount++;
+    *       } else {
+    *           fits = false;
+    *       }
+    *   }
+    *   if(state === 0) {
+    *       state = 1;
+    *       if( ( size + distanceBetween ) < width ) {
+    *           size += pointSize;
+    *       } else {
+    *           fits = false;
+    *       }
+    *   }
+    * }
+    * 
+    * so then we should put it in a function
+    * size will be or width or height of the HTML element        \/
+    * func getCapacity({pointSize, distanceBetweenPoints, size})
+    * 
+    * now we are gonna save it in constants
+    * 
+    * const dotsInRow = getCapacity({pointSize, distanceBetweenPoints, width})
+    * const dotsInColumn = getCapacity({pointSize, distanceBetweenPoints, height})
+    * 
+    * now we would like to receive coordinates of each point inside of the HTML element
+    * I would like to center this mass of points
+    * 
+    * to do this we need to know total width and height of these points
+    * 
+    * dotsInRow * pointSize + dotsInRow * distanceBetweenPoints - distanceBetweenPoints
+    *                                                removing extra margin /\
+    * 
+    * so we are gonna also put it inside of the function
+    * 
+    * func getDotsLength({dotsAmount, pointSize, distanceBetweenPoints})
+    * 
+    * and now we are able to calculate everything
+    * 
+    * const dotsWidth = getDotsLength({dotsAmount: dotsInRow, pointSize, distanceBetweenPoints})
+    * const dotsHeight = getDotsLength({dotsAmount: dotsInColumn, pointSize, distanceBetweenPoints})
+    * 
+    * const x = ( block.width - dotsWidth ) / 2;
+    * const y = ( block.height - dotsHeight ) / 2;
+    * 
+    * so "x" is an "x" of the first point
+    * and "y" is a "y" fo the first point
+    * 
+    * and now we are able to get any point position by summing and multiplying
+    * TODO: create a function that does that
+    */
+
+    return (
+        <div
+            ref={ref}
+        >
+            
+        </div>
+    );
+};
+
+export default AnimatedDots;
